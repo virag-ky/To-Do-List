b179fddbd4478670948263dc7ba9167ca697e75a
Virag Kormoczy <vkormoczy@gmail.com>
2022-03-22 17:25:21 +0200

    Add create list functionality

---
+++ modules/create-list.js
@@ -0,0 +1,39 @@
+const listContainer = document.querySelector("#list");
+
+const listObjects = [
+  {
+    description: "finish project",
+    completed: false,
+    index: 0,
+  },
+  {
+    description: "go for a walk",
+    completed: false,
+    index: 1,
+  },
+  {
+    description: "cook dinner",
+    completed: false,
+    index: 2,
+  },
+];
+
+window.addEventListener("DOMContentLoaded", () => {
+  for (let i = 0; i < listObjects.length; i += 1) {
+    const listItem = document.createElement("div");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.id = "id";
+    checkbox.value = "value";
+
+    const label = document.createElement("label");
+    label.htmlFor = "id";
+    label.appendChild(document.createTextNode(listObjects[i].description));
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+
+    listContainer.appendChild(listItem);
+  }
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import _ from "lodash";
 
 import "./style.css";
+
+import * as createList from "../modules/create-list.js";
