958ae7f5384c906c007fa23071d10d7414de98ef
Virag Kormoczy <vkormoczy@gmail.com>
2022-03-22 18:20:55 +0200

    Add basic styling

--- modules/create-list.js
+++ modules/create-list.js
@@ -1,5 +1,3 @@
-const listContainer = document.querySelector("#list");
-
 const listObjects = [
   {
     description: "finish project",
@@ -18,23 +16,4 @@ const listObjects = [
   },
 ];
 
-window.addEventListener("DOMContentLoaded", () => {
-  for (let i = 0; i < listObjects.length; i += 1) {
-    const listItem = document.createElement("div");
-    listItem.setAttribute("class", "item");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.name = "name";
-    checkbox.id = "id";
-    checkbox.value = "value";
-
-    const label = document.createElement("label");
-    label.htmlFor = "id";
-    label.appendChild(document.createTextNode(listObjects[i].description));
-
-    listItem.appendChild(checkbox);
-    listItem.appendChild(label);
-
-    listContainer.appendChild(listItem);
-  }
-});
+export default listObjects;
