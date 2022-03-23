f389c91d37f540c9215483a92d1346fc238b341a
Virag Kormoczy <vkormoczy@gmail.com>
2022-03-23 07:44:31 +0200

    Remove array of objects

--- modules/create-list.js
+++ modules/create-list.js
@@ -1,19 +1,1 @@
-const listObjects = [
-  {
-    description: 'finish project',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'go for a walk',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'cook dinner',
-    completed: false,
-    index: 2,
-  },
-];
 
-export default listObjects;
