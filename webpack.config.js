a9541f11de00d6d01f1bb8bac6ce4fea5a4f8a6d
Virag Kormoczy <vkormoczy@gmail.com>
2022-03-30 15:38:37 +0300

    Change to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
 /* eslint-disable */
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "production",
+  entry: "./src/index.js",
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'To-Do_List',
-      template: './src/index.html',
+      title: "To-Do_List",
+      template: "./src/index.html",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
